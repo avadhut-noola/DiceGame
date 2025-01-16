a340cbe4b2a5fd2bddb2f11746b0029a46a4aaaf
Avadhut Noola <avdhutnula@gmail.com>
2025-01-16 22:18:22 +0530

    Adding dice roll sound to script

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+//Generate Dice Roll Sound upon button click
+document.getElementById("rollDice").addEventListener("click", function() {
+    var audio = new Audio("audio/DiceSound.mp3");
+    audio.play();
+
+        // Delay reload for 0.6 seconds after the sound finishes
+        setTimeout(function() {
+            location.reload();
+        }, 600);
+});
+
+
+
 //Generating random numbers
 let randomNo1 = Math.floor(Math.random() * 6) + 1;
 let randomNo2 = Math.floor(Math.random() * 6) + 1;
